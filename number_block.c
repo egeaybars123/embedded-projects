9ddb8390af3fa7dc95b878a9e19e067a3fde76b4
Aegean <egeaybars1102@gmail.com>
2023-12-02 11:08:44 +0300

    alternative array_to_int

--- number_block.c
+++ number_block.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 void int_to_array(int _input, int _input1, char _arr[16]);
+void array_to_int4(int _input[4]);
+void separate_blocks(int original[], int length, int firstHalf[], int secondHalf[]);
+int array_to_int(int input[4]);
 
 int main() {
     int input[8] = {2,2,1,0,2,1,6,2};
     int input1[4]; 
     int input2[4];  
     char display[16];
+    int inp1_sum;
+    int inp2_sum;
     //array_to_int8(input);
-    //separate_blocks(input, 8, input1, input2);
+    separate_blocks(input, 8, input1, input2);
    /*  for (int i=0; i < 4; i++) {
         printf("%d,", input1[i]);
     }
@@ -16,14 +21,28 @@ int main() {
         printf("%d,", input2[i]);
     } */
 
+    inp1_sum = array_to_int(input1);
+    inp2_sum = array_to_int(input2);
+    printf("Inp1 sum: %d\n", inp1_sum);
+    printf("Inp2 sum: %d\n", inp2_sum);
+
     //array_to_int4(input1);
     //array_to_int4(input2);
 
-    int_to_array(220, 2162, display);
+    /* int_to_array(220, 2162, display);
     for (int i=0; i < 16; i++) {
         printf("%c,", display[i]);
-    }
+    } */
     return 0;
+}
+
+int array_to_int(int input[4]) {
+    int result = 0;
+    for (int i=0; i<4; i++) {
+        result *= 10;
+        result += input[i];
+    }
+    return result;
 }
 
 void int_to_array(int _input, int _input1, char _arr[16]) {
