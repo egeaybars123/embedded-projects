7995569ce79591e481c03415a4f14c89b94d7aa6
Ege Aybars Bozkurt <egeaybars1102@gmail.com>
2023-12-11 15:35:25 +0300

    second button - halt blinking

--- Project 2 - Interrupts and Timers/main.c
+++ Project 2 - Interrupts and Timers/main.c
@@ -25,11 +25,14 @@ int main(void){
 	
 }
 
-//PORTA 1- Button 1 Interrupt Pin
+//PORT A 1 - Button 1 Interrupt Pin - Green LED Blinking
+//PORT A 2 - Button 2 Interrupt Pin - Halt blinking for 2 seconds
 
 void init_button(void) {
 	PORTA->PCR[1] |= PORT_PCR_MUX(1) | PORT_PCR_PS_MASK | PORT_PCR_PE_MASK | PORT_PCR_IRQC(0x0a);
+	PORTA->PCR[2] |= PORT_PCR_MUX(1) | PORT_PCR_PS_MASK | PORT_PCR_PE_MASK | PORT_PCR_IRQC(0x0a);
 	PTA->PDDR &= ~MASK(1);
+	PTA->PDDR &= ~MASK(2);
 	
 	NVIC_SetPriority(PORTA_IRQn, 128);
 	NVIC_ClearPendingIRQ(PORTA_IRQn);
@@ -70,6 +73,9 @@ void PORTA_IRQHandler(void) {
 	
 	if(PORTA->ISFR & MASK(1)) {
 		blink_green();
+	}
+	else if(PORTA->ISFR & MASK(2)) {
+		Delay(5500000);
 	}
 	
 	//clear status flags.
