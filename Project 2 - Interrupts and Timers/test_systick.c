f082dcfd475c711962083676c7917662fa125301
Ege Aybars Bozkurt <egeaybars1102@gmail.com>
2023-12-12 13:24:18 +0300

    implemented 2t- fully working

--- Project 2 - Interrupts and Timers/test_systick.c
+++ Project 2 - Interrupts and Timers/test_systick.c
@@ -18,15 +18,12 @@ void PORTD_IRQHandler(void);
 void SysTick_Handler(void);
 void delay(unsigned milliseconds);
 
-volatile unsigned count = 0;
+static volatile unsigned count = 0;
 volatile unsigned led_count = 0;
-volatile unsigned flag_t = 0;
+volatile unsigned flag = 0;
 volatile unsigned flag_green = 0;
-volatile unsigned flag_halt = 0;
-volatile unsigned timer_count = 0;
 
 int main(void){
-	
 	//Enable clock on multiple ports
 	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
 	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
@@ -93,53 +90,72 @@ void init_counter(void) {
 void init_RGB(void) {
 	PORTB->PCR[18] &= ~PORT_PCR_MUX_MASK; //Red Cathode
 	PORTB->PCR[18] |= PORT_PCR_MUX(1);
+	PTB->PDDR |= MASK(18);
 	
+//	PORTB->PCR[19] &= ~PORT_PCR_MUX_MASK; //Green Cathode
+//	PORTB->PCR[19] |= PORT_PCR_MUX(1);
+//	PTB->PDDR |= MASK(19);
+}
+
+void blink_red(void) {
+	PORTB->PCR[18] &= ~PORT_PCR_MUX_MASK; //Red Cathode
+	PORTB->PCR[18] |= PORT_PCR_MUX(1);
+	PTB->PDDR |= MASK(18);
+	
+	while(1) {
+		PTB->PTOR |= MASK(18);
+		delay(500);
+	}
+}
+
+void blink_green(void) {
 	PORTB->PCR[19] &= ~PORT_PCR_MUX_MASK; //Green Cathode
 	PORTB->PCR[19] |= PORT_PCR_MUX(1);
+	PTB->PDDR |= MASK(19);
+	
+	//int count = 0;
+	while(led_count < 10) {
+		PTB->PTOR = MASK(19);
+		Delay(2500000);//delay(500);
+		led_count++;
+	}
+	PTB->PSOR = MASK(19);
+	led_count = 0;
 }
+
 
 void Init_SysTick(void) {
     // Initialize SysTick for your required T time
 		SysTick->CTRL = 0; 
-    SysTick->LOAD = 41000; //(uint32_t)(0.7 * SystemCoreClock) - 1;
+    SysTick->LOAD = (uint32_t)(0.7 * SystemCoreClock) - 1;
     SysTick->VAL = 0;
 		NVIC_SetPriority(SysTick_IRQn, 2);
     SysTick->CTRL |=  SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
 }
 
+void delay(unsigned milliseconds) {
+    // Initialize SysTick with appropriate settings for delays
+		SysTick->LOAD = (SystemCoreClock / 1000) * milliseconds - 1;
+    SysTick->VAL = 0;
+    SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
+
+    // Wait until the COUNTFLAG is set
+    while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
+
+    // Disable SysTick
+    SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;	
+}
+
 //***************** SYSTICK HANDLER *****************
 
 void SysTick_Handler(void) {
-	++timer_count;
-	if ((timer_count > 500) && !flag_t) {
-		PTB->PDDR = MASK(18);		
-		PTB->PTOR = MASK(18);
-		timer_count = 0;
-	}
-	
-	else if ((timer_count > 1400) && !flag_green && !flag_halt && flag_t) {
-		PTB->PDDR = MASK(18);		
-		PTB->PTOR = MASK(18);
-		timer_count = 0;
-	}
-	
-	else if ((timer_count > 300) && flag_green && !flag_halt) {
-		PTB->PDDR = MASK(19);
-		PTB->PTOR = MASK(19);
+//    if (!flag) {
+//        PTB->PTOR = MASK(18); 
+//    } else {
+//       PTB->PTOR = MASK(18);	
+//    }
 		
-		timer_count = 0;
-		led_count++;
-		
-		if (led_count > 10) {
-			flag_green = 0;
-			led_count = 0;
-		}
-	}
-	
-	else if ((timer_count > 1000) && flag_halt) {
-		flag_halt = 0;
-		timer_count = 0;
-	}
+	PTB->PTOR = MASK(18);
 }
 
 //***************** INTERRUPT HANDLERS *****************
@@ -149,13 +165,12 @@ void PORTA_IRQHandler(void) {
 	NVIC_ClearPendingIRQ(PORTA_IRQn);
 	
 	if(PORTA->ISFR & MASK(1)) {
-		flag_green = 1;
-		//blink_green();
+		blink_green();
 	}
 	else if(PORTA->ISFR & MASK(2)) {
-		flag_halt = 1;
+		Delay(2500000);//delay(2000);
 	}
-	flag_t = 1;
+	flag = 1;
 	//clear status flags.
 	PORTA->ISFR = 0xffffffff;
 }
@@ -171,7 +186,7 @@ void PORTD_IRQHandler(void) {
 	if (count == 16) {
 		count = 0;
 	}
-	flag_t = 1;
+	
 	//clear status flags.
 	PORTD->ISFR = 0xffffffff;
 }
